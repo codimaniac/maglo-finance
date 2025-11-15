86367a83c68ddbeb16d7b8e4dbf6dd5f627c3c8c
Anthony <150530087+codimaniac@users.noreply.github.com>
2025-11-15 17:40:35 +0100

    Fix: Update LoadingState.jsx
    Reduce Loading logo width for mobile device

--- src/components/custom/LoadingState.jsx
+++ src/components/custom/LoadingState.jsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/logo.svg"
 const LoadingState = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <img src={Logo} alt="Logo" className="h-auto w-100 animate-caret-blink animation-duration-[2s] ease-in" />
+      <img src={Logo} alt="Logo" className="h-auto w-70 md:w-100 animate-caret-blink animation-duration-[2s] ease-in" />
     </div>
   );
 };
